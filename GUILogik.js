ca2d3f4306259de2e80531b8fd56f75dbbdbfda7
Casper <caper.christensen@gmail.com>
2026-09-23 09:39:53 +0200

    Noget

--- GUILogik.js
+++ GUILogik.js
@@ -1,10 +1,24 @@
-import { dices, onePairScore, decreaseCounter, counter } from "./Logik.js"
+import { dices, onePairScore, twoPairScore, threeOfAKindScore, fourOfAKindScore, fullHouseScore, smallStraightScore, largeStraightScore, chanceScore, yatzyScore, decreaseCounter, counter } from "./Logik.js"
 let slåKnap = document.querySelector('#terningerKnap')
 let terninger = document.querySelectorAll('.terning')
 let checkbox = document.querySelectorAll('.cb')
 let slagTilbage = document.querySelector('#counter')
 
+let et = document.querySelector('#et')
+let to = document.querySelector('#to')
+let tre = document.querySelector('#tre')
+let fire = document.querySelector('#fire')
+let fem = document.querySelector('#fem')
+let seks = document.querySelector('#seks')
 let etPar = document.querySelector('#etPar')
+let toPair = document.querySelector('#toPar')
+let treEns = document.querySelector('#treEns')
+let fireEns = document.querySelector('#fireEns')
+let lilleStraight = document.querySelector('#lilleStraight')
+let storStraight = document.querySelector('#storStraight')
+let fuldtHus = document.querySelector('#fuldtHus')
+let chance = document.querySelector('#chancen')
+let yatzy = document.querySelector('#yatzy')
 
 let variableArray = []
 
@@ -26,7 +40,21 @@ slåKnap.addEventListener("click", function () {
 })
 
 function opdaterValues() {
+    //et.value = upperSectionScore(1)
+    //upperSectionScore(2)
+    //upperSectionScore(3)
+    //upperSectionScore(4)
+    //upperSectionScore(5)
+    //upperSectionScore(6)
     etPar.value = onePairScore()
+    toPair.value = twoPairScore()
+    //treEns.value = threeOfAKindScore()
+    //fireEns.value = fourOfAKindScore()
+    //lilleStraight.value = smallStraightScore()
+    //storStraight.value = largeStraightScore()
+    //fuldtHus.value = fullHouseScore()
+    //chance.value = chanceScore(dices)
+    //yatzy.value = yatzyScore(dices)
 }
 
 for (let v of variableArray) {
--- Logik.js
+++ Logik.js
@@ -1,5 +1,5 @@
-export let counter = 3 //tæller til at sørge for der kun er 3 slag i alt, opdateres efter hvert slag
-export const dices = []
+let counter = 3 //tæller til at sørge for der kun er 3 slag i alt, opdateres efter hvert slag
+const dices = []
 
 // øvre sektion
 let etere , toere, treere, firere, femere;
@@ -10,11 +10,11 @@ let bonus; // nødvendig?
 let etPar, toPar, treEns, fireEns, lilleStraight, storeStraight, fuldtHus, chance, Yatzy;
 let total; // nødvendig?
 
-export function decreaseCounter() {
+function decreaseCounter() {
     counter--
 }
 
-function upperSectionScore(dices, eyes) {
+function upperSectionScore(eyes) {
     let  sum = 0;
     for (let d of dices) {
         if (d === eyes) {
@@ -26,7 +26,7 @@ function upperSectionScore(dices, eyes) {
 
 // Felterne 1-6 symboliserer hver sin terning. "Count" tæller hvor mange terninger der er af hver "størrelse"
 // Feltet 0 bliver ikke brugt
-function countEyes(dices) {
+function countEyes() {
         let counts = [0,0,0,0,0,0,0];
         for (let d of dices) {
             counts[d]++;
@@ -35,8 +35,8 @@ function countEyes(dices) {
     }
 
 // Starter med højeste tal (6). Hvis hvis der findes flere terninger, vælges den. 
-export function onePairScore() {
-    let c = countEyes(dices);
+function onePairScore() {
+    let c = countEyes();
     for (let i = 6; i >= 1; i--) {
         if (c[i] >= 2) return i * 2;
     }
@@ -99,16 +99,18 @@ function fullHouseScore() {
     return (three > 0 && two > 0) ? three + two : 0;
 }
 
-function chanceScore(dices) {
+function chanceScore() {
     let sum = 0;
-    for (let d of dices) sum += d.getEyes();
+    for (let d of dices) sum += d;
     return sum;
 }
 
-function yatzyScore(dices) {
-    let first = dices[0].getEyes();
+function yatzyScore() {
+    let first = dices[0];
     for (let d of dices) {
-        if (d.getEyes() != first) return 0;
+        if (d != first) return 0;
     }
     return 50;
 }
+
+export {dices, upperSectionScore, onePairScore, twoPairScore, threeOfAKindScore, fourOfAKindScore, smallStraightScore, largeStraightScore, fullHouseScore, chanceScore, yatzyScore, decreaseCounter, counter}
